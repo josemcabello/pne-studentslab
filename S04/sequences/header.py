0765dd0f8f783c425b1eb2d18dfa2f722c999d0b
josemcab <jm.cabello.2023@alumnos.urjc.es>
2024-02-06 16:35:28 +0100

    this is my first change, but not h

---
+++ S04/sequences/header.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+FILENAME = "U5.txt"
+
+
+file_contents = Path(FILENAME).read_text()
+
+list_contents = file_contents.split("\n")
+
+for i in range(0, len(list_contents)):
+    print(list_contents[i])
