0a50c29d77f4bfc3bdb9ab4e6e412303133c3b19
josemcab <jm.cabello.2023@alumnos.urjc.es>
2024-02-20 16:42:15 +0100

    my first project

--- S07/class_ferrari.py
+++ S07/class_ferrari.py
@@ -1,16 +1,26 @@
-class Car:
+
+class Vehicle():
+    def set_speed(self, speed=0):
+        self.speed = speed
+
+class Car(Vehicle):
     def __init__(self, brand, speed=0):
         self.car_brand = brand
-        self.speed = 0
-
-    def set_speed(self, speed):
         self.speed = speed
 
 class Ferrari(Car):
-    pass
-
+    def __int__(self):
+        super().__init__("Ferrari")
+        self.music = "Classic"
+    def make_cabrio(self):
+        self.speed = 20
+        self.music = "loud"
+        return "WoW"
 
 mycar = Car("Renault")
 yourcar = Ferrari("Ferrari")
 print(yourcar.car_brand)
+yourcar.set_speed(120)
 print(yourcar.speed)
+
+print(yourcar.make_cabrio(), "The music is", yourcar.music, "and speed is", yourcar.speed)
