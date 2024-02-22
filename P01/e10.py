8492059a1dec654b881ad16c65f88ef28ebd0f69
josemcab <jm.cabello.2023@alumnos.urjc.es>
2024-02-22 15:34:34 +0100

    my first project

--- P01/e10.py
+++ P01/e10.py
@@ -7,7 +7,7 @@ s3 = Seq()
 s3.read_fasta("FXN.txt")
 s4 = Seq()
 s4.read_fasta("U5.txt")
-list_of_objects = [s1, s2, s3, s4,]
+list_of_objects = [s1, s2, s3, s4]
 list_of_files = ["ADA.txt", "FRAT1.txt", "FXN.txt", "U5.txt"]
 index = 0
 for e in list_of_objects:
