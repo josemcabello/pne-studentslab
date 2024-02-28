af24f2e847036edf65d4f74ed70f843c2e60d981
josemcab <jm.cabello.2023@alumnos.urjc.es>
2024-02-28 18:41:56 +0100

    my first project

--- P02/Client0.py
+++ P02/Client0.py
@@ -1,5 +1,26 @@
+import socket
 class Client:
     def __init__(self, ip, port):
-        print("ok")
+        self.ip = ip
+        self.port = port
     def ping(self):
-        print("OK")
+        print("OK!")
+    def __str__(self):
+        result = "Connection to SERVER at " + str(self.ip) + " PORT: " + str(self.port)
+        return result
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+        # Send data.
+        s.send(str.encode(msg))
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+        # Close the socket
+        s.close()
+        # Return the response
+        return response
+
+
+
