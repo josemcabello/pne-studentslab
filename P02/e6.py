693847aa44e4d2157e2a063b7e15bd616dd391d7
josemcab <jm.cabello.2023@alumnos.urjc.es>
2024-03-01 15:46:24 +0100

    my first project

--- P02/e6.py
+++ P02/e6.py
@@ -1,18 +1,19 @@
 from Client0 import *
 
 PRACTICE = 2
-EXERCISE = 5
+EXERCISE = 6
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "212.128.255.91" # your IP address
+IP = "212.128.255.91"# your IP address
 PORT = 1234
-
-# -- Create a client object
 c = Client(IP, PORT)
-#...
 print(c)
+PORT1 = 4321
+c1 = Client(IP, PORT1)
+print(c1)
+
 list1 = []
 s = Seq()
 s1 = s.read_fasta("FRAT1.txt")
@@ -24,16 +25,21 @@ for e in s1:
         list1.append(seq)
         seq = ""
         num += 1
-    if num == 5:
+    if num == 10:
         break
 w0 = "Sending " + str("FRAT1") + " Gene to the server, in fragments of 10 bases ..."
-w = "Gene FRAT1: " + s1
 response0 = c.talk(w0)
-response01 = c.talk(w)
+response01 = c1.talk(w0)
+w = "Gene FRAT1: " + s1
 print(w)
 num1 = 1
 for i in list1:
-    ww = "Fragment " + str(num1) + " : " + str(i)
-    response1 = c.talk(ww)
-    print(ww)
+    if num1 % 2 == 1:
+        ww = "Fragment " + str(num1) + " : " + str(i)
+        response1 = c.talk(ww)
+        print(ww)
+    else:
+        ww = "Fragment " + str(num1) + " : " + str(i)
+        response1 = c1.talk(ww)
+        print(ww)
     num1 += 1
